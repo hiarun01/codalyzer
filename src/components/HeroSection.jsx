e273925fcc370cd4efb91a83e198410f8ecc838e
hiarun01 <hiarun02@gmail.com>
2025-05-28 14:18:52 +0530

    make ui responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,23 +2,23 @@ import {Link} from "react-router-dom";
 
 const HeroSection = () => {
   return (
-    <section className="bg-white lg:grid lg:place-content-center dark:bg-black">
-      <div className="mx-auto w-screen max-w-screen-xl px-4 py-32 sm:px-6 sm:py-16 md:grid md:grid-cols-2 md:items-center sm:items-center md:gap-4 lg:px-8 lg:py-8 h-[70vh]">
-        <div className="max-w-prose text-left ">
+    <section className="bg-white lg:grid lg:place-content-center dark:bg-black lg:h-[80vh]">
+      <div className="mx-auto w-screen max-w-screen-xl px-5 py-16 sm:px-6 sm:py-16 md:grid md:grid-cols-2 md:items-center sm:items-center md:gap-4 lg:px-8 lg:py-8 lg:h-[70vh]">
+        <div className="max-w-prose text-left items-center">
           <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl dark:text-white">
             <span className="font-mono">Codalyze -</span>
             <strong className="text-red-700 "> AI Code Reviewer</strong>
           </h1>
 
-          <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed dark:text-gray-200">
+          <p className="mt-7 text-base text-pretty text-gray-700 sm:text-lg/relaxed dark:text-gray-200">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque,
             nisi. Natus, provident accusamus impedit minima harum corporis
             iusto.
           </p>
-          <div className="mt-4 flex gap-4 sm:mt-6">
+          <div className="mt-10 flex gap-4 sm:mt-6 items-center">
             <Link
-              className="inline-block rounded border border-red-600 bg-red-700 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-red-800"
               to="/dashboard"
+              className="rounded-md bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800"
             >
               Get Started
             </Link>
--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-black h-16 r border-b border-gray-800 shadow-2xs flex items-center ">
-      <div className="flex justify-between w-6xl mx-auto items-center ">
-        <div>
-          <span className="text-white text-lg font-bold">Codalyze</span>
-        </div>
+    <header className="bg-white dark:bg-black border-b border-gray-800 shadow-2xs">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex-1 md:flex md:items-center md:gap-12">
+            <span className="text-white font-bold text-lg">Codalyzer</span>
+          </div>
 
-        <div>
-          <button className="text-white bg-red-700 py-2 px-5 rounded-lg">
-            Get Started
-          </button>
+          <div className="">
+            <Link
+              to="/dashboard
+                  "
+              className="rounded-md bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800"
+            >
+              Get Started
+            </Link>
+          </div>
         </div>
       </div>
     </header>
